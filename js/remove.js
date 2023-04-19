be708d0561cb5eda380023bb95e233780d5a4c39
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2023-04-19 11:11:04 +0100

    adds removal

--- js/remove.js
+++ js/remove.js
@@ -1,18 +1,20 @@
-function onloadRemove() {
+window.Tawk_API = window.Tawk_API || {};
+window.Tawk_API.onChatMaximized = function(){
   const oneSec = 1000
-  //setInterval - for again and again
-  setTimeout(
-    function () {
-      const frames = document.getElementsByTagName("iframe");
-      if (frames) {
-        for (let index = 0; index < frames.length; index++) {
-          const frame = frames[index];
-          const innerDoc = frame.contentDocument || frame.contentWindow.document;
-          if (innerDoc)
-            removeInternalDiv(innerDoc)
-        }
-      }
-    }, (oneSec * 5));
+  setTimeout(lookInAllIframe, (oneSec * 0.2));
+  setTimeout(lookInAllIframe, (oneSec * 1));
+};
+
+function lookInAllIframe() {
+  const frames = document.getElementsByTagName("iframe");
+  if (frames) {
+    for (let index = 0; index < frames.length; index++) {
+      const frame = frames[index];
+      const innerDoc = frame.contentDocument || frame.contentWindow.document;
+      if (innerDoc)
+        removeInternalDiv(innerDoc)
+    }
+  }
 }
 
 function removeInternalDiv(doc) {
