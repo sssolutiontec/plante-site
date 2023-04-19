20b6e32ae6dcf542bd8f1cc5982aaedaa1e62c09
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2023-04-19 12:02:09 +0100

    already opened and page refreshed

--- js/remove.js
+++ js/remove.js
@@ -1,8 +1,11 @@
 window.Tawk_API = window.Tawk_API || {};
+window.Tawk_API.onLoad = function(){
+  setTimeout(lookInAllIframe, (oneSec * 0.2));
+};
 window.Tawk_API.onChatMaximized = function(){
   const oneSec = 1000
   setTimeout(lookInAllIframe, (oneSec * 0.1));
-  setTimeout(lookInAllIframe, (oneSec * 0.3));
+  setTimeout(lookInAllIframe, (oneSec * 0.4));
   setTimeout(lookInAllIframe, (oneSec * 1));
 };
 
